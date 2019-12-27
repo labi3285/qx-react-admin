f99cc3a221c2923723df17eb62516c982c3e27e3
lzh <liuzhenhui@lakala.com>
2019-12-27 11:41:49 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import Page1 from './routes/page1'
+
+const App = () => {
+  // import('./routes/page1')
+  // .then(comp => {
+  //   console.log("aaa" + comp);
+  // })
+  // .catch(error => {
+  //   console.log("eee" + error);
+  // })
+
+  // let Page1 = null
+  //import(/* webpackChunkName: "page1" */'./routes/page1').then(comp => {
+  //   Page1 = comp
+  //})
+  // <Page1 />
+
+  return (
+    <div>
+      <div>App2</div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/routes/page2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import _ from 'lodash'
+const Page2 = () => {
+  return (
+    <div>Page2</div>
+  )
+}
+
+export default Page2
